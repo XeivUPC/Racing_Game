17df98218d16644aaf495789e4d0e7ef63ba2528
Xeiv360 <adria.garcia.almaden@estudiantat.upc.edu>
2024-12-11 22:28:51 +0100

    FixRender

--- Source/ModuleRender.h
+++ Source/ModuleRender.h
@@ -3,10 +3,26 @@
 #include "Globals.h"
 
 #include <limits.h>
+#include <vector>
 
 class ModuleRender : public Module
 {
 public:
+	enum RenderLayer {
+		SUB_LAYER_5,
+		SUB_LAYER_4,
+		SUB_LAYER_3,
+		SUB_LAYER_2,
+		SUB_LAYER_1,
+		DEFAULT,
+		OVER_LAYER_1,
+		OVER_LAYER_2,
+		OVER_LAYER_3,
+		OVER_LAYER_4,
+		OVER_LAYER_5,
+		DEBUG_LAYER_DO_NOT_USE
+	};
+
 	ModuleRender(Application* app, bool start_enabled = true);
 	~ModuleRender();
 
@@ -17,10 +33,6 @@ public:
 	bool CleanUp();
 
     void SetBackgroundColor(Color color);
-<<<<<<< Updated upstream
-	bool Draw(Texture2D texture, int x, int y, const Rectangle* section = NULL, double angle = 0, int pivot_x = 0, int pivot_y = 0, Color tint = WHITE) const;
-    bool DrawText(const char* text, int x, int y, Font font, int spacing, Color tint) const;
-=======
 	bool Draw(Texture2D texture, int x, int y, const Rectangle* section = NULL, double angle = 0, float scale =1, int pivot_x = 0, int pivot_y = 0, Color tint = WHITE) ;
     bool DrawText(const char* text, int x, int y, Font font, int spacing, Color tint);
 
@@ -36,10 +48,25 @@ public:
 
 	void ResetRenderLayer();
 
->>>>>>> Stashed changes
 
 public:
 
 	Color background;
     Rectangle camera;
+
+private:
+
+	struct Layer {
+		RenderLayer layer;
+		RenderTexture2D data;
+	};
+
+	void RenderLayerToScreen(Layer layer);
+	void CreateLayer(RenderLayer layer);
+	void CleanRenderLayer(Layer layer);
+	
+
+	std::vector<Layer> layers;
+	RenderLayer currentRenderLayer = DEFAULT;
+	bool blockRenderLayer = false;
 };
