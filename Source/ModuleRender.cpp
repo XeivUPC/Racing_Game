e99904ff913630ecc9ded1d278c8c770c32e3b29
Xeiv360 <adria.garcia.almaden@estudiantat.upc.edu>
2024-12-11 15:09:22 +0100

    Layer Based Render

--- Source/ModuleRender.cpp
+++ Source/ModuleRender.cpp
@@ -7,6 +7,8 @@
 ModuleRender::ModuleRender(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
     background = BLACK;
+
+   
 }
 
 // Destructor
@@ -19,24 +21,38 @@ bool ModuleRender::Init()
 	LOG("Creating Renderer context");
 	bool ret = true;
 
+    for (size_t i = 0; i < DEBUG_LAYER_DO_NOT_USE; i++)
+    {
+        CreateLayer((RenderLayer)i);
+    }
+
 	return ret;
 }
 
 // PreUpdate: clear buffer
 update_status ModuleRender::PreUpdate()
 {
+    ClearBackground(background);
+    for (size_t i = 0; i < layers.size(); i++)
+    {
+        CleanRenderLayer(layers[i]);
+    }
 	return UPDATE_CONTINUE;
 }
 
 // Update: debug camera
 update_status ModuleRender::Update()
 {
-    ClearBackground(background);
+   
 
     // NOTE: This function setups render batching system for
     // maximum performance, all consecutive Draw() calls are
     // not processed until EndDrawing() is called
     BeginDrawing();
+    for (size_t i = 0; i < layers.size(); i++)
+    {
+        RenderLayerToScreen(layers[i]);
+    }
 
 	return UPDATE_CONTINUE;
 }
@@ -44,7 +60,9 @@ update_status ModuleRender::Update()
 // PostUpdate present buffer to screen
 update_status ModuleRender::PostUpdate()
 {
+
     // Draw everything in our batch!
+    
     DrawFPS(10, 10);
 
     EndDrawing();
@@ -55,6 +73,11 @@ update_status ModuleRender::PostUpdate()
 // Called before quitting
 bool ModuleRender::CleanUp()
 {
+    for (size_t i = 0; i < layers.size(); i++)
+    {
+        UnloadRenderTexture(layers[i].data);
+    }
+    layers.clear();
 	return true;
 }
 
@@ -64,8 +87,9 @@ void ModuleRender::SetBackgroundColor(Color color)
 }
 
 // Draw to screen
-bool ModuleRender::Draw(Texture2D texture, int x, int y, const Rectangle* section, double angle, int pivot_x, int pivot_y, Color tint ) const
+bool ModuleRender::Draw(Texture2D texture, int x, int y, const Rectangle* section, double angle, int pivot_x, int pivot_y, Color tint )
 {
+    BeginTextureMode(layers.at(currentRenderLayer).data);
     bool ret = true;
 
     float scale = 1.0f;
@@ -83,17 +107,81 @@ bool ModuleRender::Draw(Texture2D texture, int x, int y, const Rectangle* sectio
 
     // Rotate the texture using DrawTexturePro with the correct parameters
     DrawTexturePro(texture, rect, destRect, pivot, (float)angle, tint);
-
+    EndTextureMode();
+    if (!IsRenderLayerBlocked())
+        ResetRenderLayer();
 	return ret;
 }
 
-bool ModuleRender::DrawText(const char * text, int x, int y, Font font, int spacing, Color tint) const
+bool ModuleRender::DrawText(const char * text, int x, int y, Font font, int spacing, Color tint)
 {
+    BeginTextureMode(layers.at(currentRenderLayer).data);
     bool ret = true;
 
     Vector2 position = { (float)x, (float)y };
 
     DrawTextEx(font, text, position, (float)font.baseSize, (float)spacing, tint);
-
+    EndTextureMode();
+    if (!IsRenderLayerBlocked())
+        ResetRenderLayer();
     return ret;
+}
+
+void ModuleRender::SelectRenderLayer(RenderLayer layer)
+{
+    currentRenderLayer = layer;
+}
+
+ModuleRender::RenderLayer ModuleRender::GetCurrentRenderLayer()
+{
+    return currentRenderLayer;
+}
+
+void ModuleRender::BlockRenderLayer(RenderLayer layer)
+{
+    BlockRenderLayer();
+    SelectRenderLayer(layer);
+}
+
+void ModuleRender::BlockRenderLayer()
+{
+    blockRenderLayer = true;
+}
+
+void ModuleRender::UnlockRenderLayer()
+{
+    blockRenderLayer = false;
+    ResetRenderLayer();
+}
+
+bool ModuleRender::IsRenderLayerBlocked()
+{
+    return blockRenderLayer;
+}
+
+void ModuleRender::ResetRenderLayer()
+{
+    currentRenderLayer = DEFAULT;
+}
+
+void ModuleRender::RenderLayerToScreen(Layer layer)
+{
+    Rectangle sourceRect = { 0, 0, (float)layer.data.texture.width, (float)-layer.data.texture.height };
+    Rectangle destRect = { 0, 0, (float)SCREEN_WIDTH, (float)SCREEN_HEIGHT };
+
+    // Draw the layer's texture to the screen, flipping vertically
+    DrawTexturePro(layer.data.texture, sourceRect, destRect, { 0, 0 }, 0, WHITE);
+}
+
+void ModuleRender::CreateLayer(RenderLayer layer)
+{
+    RenderTexture2D texture = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
+    layers.push_back({layer, texture});
+}
+
+void ModuleRender::CleanRenderLayer(Layer layer)
+{
+    BeginTextureMode(layer.data);
+    ClearBackground(BLANK);
+    EndTextureMode();
 }
--- Source/ModuleRender.h
+++ Source/ModuleRender.h
@@ -3,10 +3,26 @@
 #include "Globals.h"
 
 #include <limits.h>
+#include <vector>
 
 class ModuleRender : public Module
 {
 public:
+	enum RenderLayer {
+		SUB_LAYER_5,
+		SUB_LAYER_4,
+		SUB_LAYER_3,
+		SUB_LAYER_2,
+		SUB_LAYER_1,
+		DEFAULT,
+		OVER_LAYER_1,
+		OVER_LAYER_2,
+		OVER_LAYER_3,
+		OVER_LAYER_4,
+		OVER_LAYER_5,
+		DEBUG_LAYER_DO_NOT_USE
+	};
+
 	ModuleRender(Application* app, bool start_enabled = true);
 	~ModuleRender();
 
@@ -17,11 +33,40 @@ public:
 	bool CleanUp();
 
     void SetBackgroundColor(Color color);
-	bool Draw(Texture2D texture, int x, int y, const Rectangle* section = NULL, double angle = 0, int pivot_x = 0, int pivot_y = 0, Color tint = WHITE) const;
-    bool DrawText(const char* text, int x, int y, Font font, int spacing, Color tint) const;
+	bool Draw(Texture2D texture, int x, int y, const Rectangle* section = NULL, double angle = 0, int pivot_x = 0, int pivot_y = 0, Color tint = WHITE) ;
+    bool DrawText(const char* text, int x, int y, Font font, int spacing, Color tint);
+
+	///Layers
+
+	void SelectRenderLayer(RenderLayer layer);
+	RenderLayer GetCurrentRenderLayer();
+
+	void BlockRenderLayer(RenderLayer layer);
+	void BlockRenderLayer();
+	void UnlockRenderLayer();
+	bool IsRenderLayerBlocked();
+
+	void ResetRenderLayer();
+
 
 public:
 
 	Color background;
     Rectangle camera;
+
+private:
+
+	struct Layer {
+		RenderLayer layer;
+		RenderTexture2D data;
+	};
+
+	void RenderLayerToScreen(Layer layer);
+	void CreateLayer(RenderLayer layer);
+	void CleanRenderLayer(Layer layer);
+	
+
+	std::vector<Layer> layers;
+	RenderLayer currentRenderLayer = DEFAULT;
+	bool blockRenderLayer = false;
 };
