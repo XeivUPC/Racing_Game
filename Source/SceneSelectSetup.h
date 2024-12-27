5d7e36217fc8dde9f47a8b4e90bde7f5e150fa5c
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2024-12-27 23:44:05 +0100

    Car select initial

--- Source/SceneSelectSetup.h
+++ Source/SceneSelectSetup.h
@@ -29,8 +29,20 @@ public:
 	};
 
 	// Choosing Map
+	enum MAPS {
+		MAP1,
+		MAP2,
+		MAP3
+	};
 
 	// Choosing Car Type
+	enum VEHICLES {
+		NONE,
+		CAR,
+		MOTO,
+		TRUCK,
+		END
+	};
 
 
 private:
@@ -67,5 +79,32 @@ private:
 	// Choosing Map
 	
 	// Choosing Car Type
+
+	void ClickFINISH();
+	void OnMouseOverFINISH();
+
+	void ClickCarRightArrow();
+
+	void ClickCarLeftArrow();
+
+	Texture2D* car_bg_texture = nullptr;
+	Rectangle  car_bg_textureRec = { 0, 0, 640, 360 };
+
+	UIButton* finish_car_button = nullptr;
+	Rectangle finish_car_button_textureRec = { (282 * 2), (240 * 2), 304 * 2, 64 * 2 };
+	Texture2D* finish_car_button_texture_hover = nullptr;
+	Rectangle  finish_car_button_texture_hover_section = { 0, 0, 304, 64 };
+	Vector2 finish_car_button_textOffset = { 80, 7 };
+
+	Vector2 car_name_text_pos = { 141 * 2 , 283 * 2 };
+
+	UIButton* car_arrow_right = nullptr;
+	Rectangle car_arrow_right_rec = { (233 * 2), (164 * 2), 15 * 2, 13 * 2 };
+	UIButton* car_arrow_left = nullptr;
+	Rectangle car_arrow_left_rec = { (36 * 2), (164 * 2), 15 * 2, 13 * 2 };
+
+	bool isCarChosen = false;
+	VEHICLES currentVEHICLE = VEHICLES::CAR;
+	VEHICLES finalVEHICLE = VEHICLES::NONE;
 
 };
